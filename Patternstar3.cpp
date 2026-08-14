98371465ceb942e674fcddce5f8d504b4b076ffc
Utsav <uttsavchauhan@gmail.com>
2026-08-14 19:28:16 +0530

    Replace Armstrong number check with star pattern

--- Patternstar3.cpp
+++ Patternstar3.cpp
@@ -1,39 +1,19 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	for(int i=1;i<=4;i++)
-//	{
-//		for(int j=1;j<=4-i;j++)
-//		{
-//			cout<<" ";
-//		}
-//		for(int k=1;k<=2*i-1;k++)
-//		{
-//			cout<<"*";	
-//		}
-//		cout<<endl;
-//	}
-//}
-
 #include<iostream>
 using namespace std;
 int main()
 {
-	int num=153;
-	int num1=153;
-	int sum=0;
-	
-	while(num)
+	for(int i=1;i<=4;i++)
 	{
-		int rem=num%10;
-		int cube = rem*rem*rem;
-		sum=cube+sum;
-		num=num/10;
+		for(int j=1;j<=4-i;j++)
+		{
+			cout<<" ";
+		}
+		for(int k=1;k<=2*i-1;k++)
+		{
+			cout<<"*";	
+		}
+		cout<<endl;
 	}
-	if (sum==num1)
-	{
-		cout<<"Armstrong";
-	}
-	return 0;
 }
+
+
